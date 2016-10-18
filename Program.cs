5b773dbbd9c6c099f6bf2f89fbe7d5ae40875952
Erick <NBA_EAP@hotmail.com>
2016-10-17 18:44:10 -0600

    made changes

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        public static MyRobotStats j42;//makes new object.
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            j42.DisplayPower();
+            int myNumber = 21;
+            int yourNumber = 19;
+            int newNumber = myNumber + yourNumber;
+            string firstLine = "Hello World! This is my first C# project.";
+            Console.WriteLine(firstLine);
+            Console.WriteLine(newNumber);
         }
     }
 }
---
+++ RobotStats.cs
@@ -0,0 +1,11 @@
+using System;
+public class MyRobotStats {//object with properties
+    int power = 10;
+    int ammo = 5;
+    string armor = "Steal";
+
+    public void DisplayPower () {//A function/method
+        //displays it on the console.
+        Console.WriteLine("Robot has 10 power");
+    }
+}
